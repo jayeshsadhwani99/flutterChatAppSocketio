4c012778b88d71090d78af8457f48644f0259e9e
unknown <jayeshsadhwani99@gmail.com>
2021-06-08 14:47:13 +0530

    Add custom font

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        fontFamily: "OpenSans",
         primaryColor: Color(0xFF075E54),
         accentColor: Color(0xFF128C7E),
       ),
