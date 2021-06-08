80c0dbe029ca523789aa9748939a7bec14fd756c
unknown <jayeshsadhwani99@gmail.com>
2021-06-08 15:08:25 +0530

    Custom SVG and divider

--- lib/CustomUI/CustomCard.dart
+++ lib/CustomUI/CustomCard.dart
@@ -1,36 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomCard extends StatelessWidget {
   const CustomCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-      ),
-      title: Text(
-        "Jayesh",
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Row(
+    return InkWell(
+      onTap: () {},
+      child: Column(
         children: [
-          Icon(Icons.done_all),
-          SizedBox(
-            width: 3,
-          ),
-          Text(
-            "Hello World!",
-            style: TextStyle(
-              fontSize: 13,
+          ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: SvgPicture.asset(
+                "assets/groups.svg",
+                color: Colors.white,
+                height: 37,
+                width: 37,
+              ),
+              backgroundColor: Colors.blueGrey,
             ),
-          )
+            title: Text(
+              "Jayesh",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(Icons.done_all),
+                SizedBox(
+                  width: 3,
+                ),
+                Text(
+                  "Hello World!",
+                  style: TextStyle(
+                    fontSize: 13,
+                  ),
+                )
+              ],
+            ),
+            trailing: Text("18:04"),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20, left: 80),
+            child: Divider(
+              thickness: 1,
+            ),
+          ),
         ],
       ),
-      trailing: Text("18:04"),
     );
   }
 }
