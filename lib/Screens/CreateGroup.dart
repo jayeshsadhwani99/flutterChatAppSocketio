86aaddb356167761e02dd0af95566692300fe118
unknown <jayeshsadhwani99@gmail.com>
2021-06-09 14:38:39 +0530

    Group page done

--- lib/Screens/CreateGroup.dart
+++ lib/Screens/CreateGroup.dart
@@ -1,3 +1,4 @@
+import 'package:chatappsocketio/CustomUI/AvatarCard.dart';
 import 'package:chatappsocketio/CustomUI/ButtonCard.dart';
 import 'package:chatappsocketio/CustomUI/ContactCard.dart';
 import 'package:chatappsocketio/Model/ChatModel.dart';
@@ -69,26 +70,72 @@ class _CreateGroupState extends State<CreateGroup> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: contacts.length,
-        itemBuilder: (context, index) => InkWell(
-          onTap: () {
-            if (contacts[index].select == false) {
-              setState(() {
-                contacts[index].select = true;
-                groups.add(contacts[index]);
-              });
-            } else {
-              setState(() {
-                contacts[index].select = false;
-                groups.remove(contacts[index]);
-              });
-            }
-          },
-          child: ContactCard(
-            contact: contacts[index],
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: contacts.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return Container(
+                  height: groups.length > 0 ? 90 : 0,
+                );
+              }
+
+              return InkWell(
+                onTap: () {
+                  if (contacts[index].select == false) {
+                    setState(() {
+                      contacts[index - 1].select = true;
+                      groups.add(contacts[index - 1]);
+                    });
+                  } else {
+                    setState(() {
+                      contacts[index - 1].select = false;
+                      groups.remove(contacts[index - 1]);
+                    });
+                  }
+                },
+                child: ContactCard(
+                  contact: contacts[index - 1],
+                ),
+              );
+            },
           ),
-        ),
+          groups.length > 0
+              ? Column(
+                  children: [
+                    Container(
+                      height: 75,
+                      color: Colors.white,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: contacts.length,
+                        itemBuilder: (context, index) {
+                          if (contacts[index].select == true) {
+                            return InkWell(
+                              onTap: () {
+                                setState(() {
+                                  contacts[index].select = false;
+                                  groups.remove(contacts[index]);
+                                });
+                              },
+                              child: AvatarCard(
+                                contact: contacts[index],
+                              ),
+                            );
+                          } else {
+                            return Container();
+                          }
+                        },
+                      ),
+                    ),
+                    Divider(
+                      thickness: 1,
+                    )
+                  ],
+                )
+              : Container(),
+        ],
       ),
     );
   }
